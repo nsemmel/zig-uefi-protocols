eb97496ccfec5484deda7b0b953b4ae8a536299a
Neil Semmel <40726689+nsemmel@users.noreply.github.com>
2025-11-18 14:31:10 -0600

    Add Pxe protocol import

--- src/protocol.zig
+++ src/protocol.zig
@@ -2,10 +2,12 @@ pub const Dhcp4 = @import("protocol/dhcp4.zig").Dhcp4;
 pub const Ip4 = @import("protocol/ip4.zig").Ip4;
 pub const Tcp4 = @import("protocol/tcp4.zig").Tcp4;
 pub const Http = @import("protocol/http.zig").Http;
+pub const Pxe = @import("protocol/pxe.zig").Pxe;
 
 test {
     _ = Dhcp4;
     _ = Ip4;
     _ = Tcp4;
     _ = Http;
+    _ = Pxe;
 }
